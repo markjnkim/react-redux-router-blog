739bc130ee8e959349d2a8f0fdaaeae470e07dd3
trumpocalyspe <mark.jn.kim@gmail.com>
2018-11-13 13:03:13 -0800

    added redux form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {fetchPosts } from '../actions';
+import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
-export default class PostsIndex extends Component {
+
+class PostsIndex extends Component {
+  // Lifecycle Method 
+  // Always runs once after render
+  // ex. fetch data async so b4 or after doesn't actually matter
+  componentDidMount() {
+    // data call
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>{post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <div className="text-xs-right">
+        {/* anchor tag in React */}
+        <Link className="btn btn-primary" to="/posts/new">
+          Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );  
   }
 }; 
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+// same as using mapDispatchToProps
+// abbreviated syntax sugar, however not good if modifications are necessary
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default props => {
-  return (
-    <div>
-
-    </div>
-  );
-}; 
+export default class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Title</h3>
+        <form action=""></form>
+        <h3>Categories</h3>
+        <form action=""></form>
+        <h3>Content</h3>
+        <form action=""></form>
+      </div>
+    );
+  }
+}
