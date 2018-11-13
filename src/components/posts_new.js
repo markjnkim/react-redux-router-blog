e5373f7afce0dfaa9a7d68cac100fcd1b6d6c021
trumpocalyspe <mark.jn.kim@gmail.com>
2018-11-13 14:08:10 -0800

    added field forms for blog post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,18 +2,52 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form'; 
 
 class PostsNew extends Component {
-  render() {
+ 
+  // field is the component Field has event handler to write up JSX Field component
+  renderField(field) {
     return (
-      <div>
-        <h3>Title</h3>
-        <form action=""></form>
-        <h3>Categories</h3>
-        <form action=""></form>
-        <h3>Content</h3>
-        <form action=""></form>
+      <div className="form-group">
+      <label >{field.label}</label>
+        <input
+          className="form-control"
+          // makes available as props all the event handlers from input
+          type="text"
+          {...field.input}
+        />
       </div>
     );
   }
-}
 
-export default reduxForm()(PostsNew);
+  render() {
+    return (
+      <form action="">
+      {/* 3 field for each form 
+      Add props that passes in through field obj */}
+        <Field 
+          label="Title"
+          name="title"
+          component={this.renderField}
+
+        />
+        <Field 
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field 
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
+}
+// similar to the connect helper now handled by reduxForm
+// possible to display multiple forms \/
+// Make sure value in object is a unique string
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
+
+// PostsEdit.js
